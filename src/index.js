19510f04a0cca11df7a10d20ce11185aeddf3db8
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2024-11-05 18:52:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+//Required for hot loading HTML changes.
 require("../index.html");
+// require("../style.css")
+import "../style.css";
 
-console.log("Hello World!");
+//Variables.
+var ele_mainPage;
+var btn_startClient;
+var btn_startServer;
+
+//Initialize document elements to variables.
+document.body.querySelectorAll("*").forEach(element =>
+{
+    switch(element.getAttribute("class"))
+    {
+        case "mainpage":
+            ele_mainPage = element;
+            break;
+
+        case "startclient":
+            btn_startClient = element;
+            break;
+
+        case "startserver":
+            btn_startServer = element;
+            break;
+    }
+});
