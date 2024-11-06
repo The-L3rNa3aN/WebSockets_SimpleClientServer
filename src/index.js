9ebaf51d34411aae96532df8491976657e449bc5
Veman Jadhav <54273732+The-L3rNa3aN@users.noreply.github.com>
2024-11-06 18:24:00 +0530

    Update index.js
    Functionality for changing the "mode-heading" innerHTML accordingly.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 //Required for hot loading HTML changes.
 require("../index.html");
-// require("../style.css")
-import "../style.css";
+require("../style.css")
 
 //Variables.
 var ele_mainPage;
 var btn_startClient;
 var btn_startServer;
+var ele_modeHeading;
 
 //Initialize document elements to variables.
 document.body.querySelectorAll("*").forEach(element =>
@@ -24,5 +24,24 @@ document.body.querySelectorAll("*").forEach(element =>
         case "startserver":
             btn_startServer = element;
             break;
+            
+        case "mode-heading":
+            ele_modeHeading = element;
+            break;
     }
 });
+
+btn_startServer.addEventListener("click", () =>
+{
+    setPageHeading(0);
+});
+
+btn_startClient.addEventListener("click", () =>
+{
+    setPageHeading(1);
+});
+
+function setPageHeading(i)
+{
+    ele_modeHeading.innerHTML = i === 0 ? "SERVER" : "CLIENT";
+}
