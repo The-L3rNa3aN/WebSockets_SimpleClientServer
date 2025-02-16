26ea628b28b6f031e0f495b56403583b623a5219
Veman Jadhav <vemancraft@gmail.com>
2025-02-16 19:18:33 +0530

    Update index.js
    I don't know what I was working on. Last commit.

--- client/index.js
+++ client/index.js
@@ -9,7 +9,12 @@ exapp.get("/", (req, res) =>
 {
     const options = { root: path.join("./") };
     res.sendFile("index.html", options);
+
+    // console.log(req.socket.remoteAddress);
+    console.log(req.ip);
+    res.status(200).send(`<p>${req.ip}</p>`);
 });
+
 exapp.use(express.static("./"));
 exapp.listen(PORT);
 open("http://localhost:" + PORT);
